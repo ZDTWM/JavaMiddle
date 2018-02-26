da1db204977eb226f77af39f66cd422f09a2da83
TWM <480086311@qq.com>
2018-02-26 14:08:14 +0800

    什么是异常： 导致程序的正常流程被中断的事件，叫做异常

---
+++ src/abnormal/Demo01.java
@@ -0,0 +1,34 @@
+package abnormal;
+
+import java.io.File;
+import java.io.FileInputStream;
+
+/**
+ * 什么是异常？
+ * @author Administrator
+ * 异常定义： 导致程序的正常流程被中断的事件，叫做异常
+ */
+public class Demo01 {
+	/**
+	 * 1、文件不存在异常
+	 */
+	//比如要打开d盘的LOL.exe文件，这个文件是有可能不存在的 
+	//Java中通过 new FileInputStream(f) 试图打开某文件，就有可能抛出文件不存在异常FileNotFoundException 
+	//如果不处理该异常，就会有编译错误 
+	public static void main(String[] args) {
+		File f = new File("d:/LOL.exe");
+		//试图打开文件LOL.exe，会抛出FileNotFoundException，如果不处理该异常，就会有编译错误
+		//new FileInputStream(f);
+		
+	}
+	
+	/**
+	 * 2、常见异常
+	 */
+	//ParseException 解析异常，日期字符串转换为日期对象的时候，有可能抛出的异常
+	//OutOfIndexException 数组下标越界异常
+	//OutOfMemoryError 内存不足
+	//ClassCastException 类型转换异常
+	//ArithmaticException 除数为零
+	//NullPointerException 空指针异常
+}
