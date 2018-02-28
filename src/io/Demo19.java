a02871cbe4c942f33a493fe40e7d2b73a6dc6f4c
TWM <480086311@qq.com>
2018-02-28 11:49:14 +0800

    一个汉字使用不同编码方式的表现

---
+++ src/io/Demo19.java
@@ -0,0 +1,41 @@
+package io;
+
+import java.io.UnsupportedEncodingException;
+
+/**
+ * 一个汉字使用不同编码方式的表现
+ * @author Administrator
+ *
+ */
+//以字符 中 为例，查看其在不同编码方式下的值是多少
+//也即在不同的棋盘上的位置
+public class Demo19 {
+    public static void main(String[] args) {
+        String str = "中";
+        showCode(str);
+    }
+ 
+    private static void showCode(String str) {
+        String[] encodes = { "BIG5", "GBK", "GB2312", "UTF-8", "UTF-16", "UTF-32" };
+        for (String encode : encodes) {
+            showCode(str, encode);
+        }
+ 
+    }
+ 
+    private static void showCode(String str, String encode) {
+        try {
+            System.out.printf("字符: \"%s\" 的在编码方式%s下的十六进制值是%n", str, encode);
+            byte[] bs = str.getBytes(encode);
+ 
+            for (byte b : bs) {
+                int i = b&0xff;
+                System.out.print(Integer.toHexString(i) + "\t");
+            }
+            System.out.println();
+            System.out.println();
+        } catch (UnsupportedEncodingException e) {
+            System.out.printf("UnsupportedEncodingException: %s编码方式无法解析字符%s\n", encode, str);
+        }
+    }
+}
