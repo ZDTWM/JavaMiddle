34089f2b229ba00b3113aefeb09f014df902acc1
TWM <480086311@qq.com>
2018-03-13 09:07:16 +0800

    对象流

---
+++ src/io/Demo28.java
@@ -0,0 +1,42 @@
+package io;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+public class Demo28 {
+
+	public static void main(String[] args) {
+        //创建一个Hero garen
+        //要把Hero对象直接保存在文件上，务必让Hero类实现Serializable接口
+		Hero h = new Hero();
+		h.name = "garen";
+		h.hp = 616;
+		
+		File f = new File("d:/garen.lol");
+		
+		try(
+				//创建对象输出流
+				FileOutputStream fos = new FileOutputStream(f);
+				ObjectOutputStream oos = new ObjectOutputStream(fos);
+				//创建对象输入流
+				FileInputStream fis = new FileInputStream(f);
+				ObjectInputStream ois = new ObjectInputStream(fis);
+		) {
+			oos.writeObject(h);
+			Hero h2 = (Hero) ois.readObject();
+			System.out.println(h2.name);
+			System.out.println(h2.hp);
+		} catch (IOException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		} catch(ClassNotFoundException e){
+			e.printStackTrace();
+		}
+		
+	}
+
+}
