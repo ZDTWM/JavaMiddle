2d5ecdf0e9241cab6c6c3c440b4f7e9e6abad953
TWM <480086311@qq.com>
2018-03-13 10:07:29 +0800

    使用数组的局限性

---
+++ src/collection/framework/Demo01.java
@@ -0,0 +1,16 @@
+package collection.framework;
+
+public class Demo01 {
+	
+	public static void main(String[] args) {
+		//数组的局限性
+		Hero heros[] = new Hero[10];
+        //声明长度是10的数组
+        //不用的数组就浪费了
+        //超过10的个数，又放不下
+		heros[0] = new Hero("盖伦");
+		//放不下要报错
+		heros[20] = new Hero("提莫");
+	}
+	
+}
